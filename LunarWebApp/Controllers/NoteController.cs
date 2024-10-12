3b133067a65034a7cda6521dd737c5ffe64bd4f9
dpiskov <dpiskov02@gmail.com>
2024-10-12 21:57:48 +0300

    Added Note model, NoteController and coresponding views

---
+++ LunarWebApp/Controllers/NoteController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using LunarWebApp.Models; // Include the Note model
+
+namespace LunarWebApp.Controllers
+{
+    public class NoteController : Controller
+    {
+        //In-memory list for now (will replace with database later)
+        private static List<Note> notes = new List<Note>();
+
+        //Action to show all notes
+        public IActionResult Index()
+        {
+            return View(notes); //Pass the list of notes to the view
+        }
+
+        //GET: Action to display form for creating a new note
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Note note)
+        {
+            //Assign a unique id to the note
+            note.Id = Guid.NewGuid();
+
+            //Add the note to the list
+            notes.Add(note);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        //Action to show details of a specific note
+        public IActionResult Details(Guid id)
+        {
+            //Find the note with the given id
+            Note note = notes.Find(n => n.Id == id);
+
+            if (note == null)
+            {
+                return NotFound(); // Return 404 Not Found
+            }
+
+            return View(note);
+        }
+    }
+}
---
+++ LunarWebApp/Models/Note.cs
@@ -0,0 +1,19 @@
+﻿namespace LunarWebApp.Models
+{
+    public class Note
+    {
+        public Note()
+        {
+            DateCreated = DateTime.Now;
+            ;
+        }
+
+        public Guid Id { get; set; }
+        public required string Title { get; set; }
+        public required string Body { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime LastOpened { get; set; }
+
+
+    }
+}
