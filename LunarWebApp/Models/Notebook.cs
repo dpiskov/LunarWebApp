dc6e7602b97cec3ac4255d4810fec3440541555a
dpiskov <dpiskov02@gmail.com>
2024-10-13 09:59:12 +0300

    Added a Notebook model

---
+++ LunarWebApp/Models/Notebook.cs
@@ -0,0 +1,10 @@
+﻿namespace LunarWebApp.Models
+{
+    public class Notebook
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public  required ICollection<Folder> Folders { get; set; }
+        public  required ICollection<Note> Notes { get; set; }
+    }
+}
