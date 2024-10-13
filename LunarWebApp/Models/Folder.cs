d7fd4a9ca85ddc53bd8c53b64faa14b6ba2e212c
dpiskov <dpiskov02@gmail.com>
2024-10-13 09:59:54 +0300

    Added a Folder model

---
+++ LunarWebApp/Models/Folder.cs
@@ -0,0 +1,14 @@
+﻿namespace LunarWebApp.Models
+{
+    public class Folder
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public Guid NotebookId { get; set; }
+        public required Notebook Notebook { get; set; }
+        public Guid ParentFolderId { get; set; }
+        public required Folder ParentFolder { get; set; }
+        public required ICollection<Folder> Folders { get; set; }
+        public required ICollection<Note> Notes { get; set; }
+    }
+}
