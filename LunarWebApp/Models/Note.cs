7d9b253aa6164dd9a939ae779bc889f5bf9f3b26
dpiskov <dpiskov02@gmail.com>
2024-10-13 09:59:32 +0300

    Edited the Note model

--- LunarWebApp/Models/Note.cs
+++ LunarWebApp/Models/Note.cs
@@ -5,12 +5,15 @@
         public Note()
         {
             DateCreated = DateTime.Now;
-            ;
         }
 
         public Guid Id { get; set; }
         public required string Title { get; set; }
         public required string Body { get; set; }
+        public Guid NotebookId { get; set; }
+        public Notebook Notebook { get; set; }
+        public Guid FolderId { get; set; }
+        public Folder Folder { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime LastOpened { get; set; }
 
