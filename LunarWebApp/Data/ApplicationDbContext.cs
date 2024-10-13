1ca87e56332f9d6f0d5c44a9b81f38353d6cf8f7
dpiskov <dpiskov02@gmail.com>
2024-10-13 11:09:14 +0300

    Added ApplicationDbContext with DbSets for Notebook, Folder, and Note. Configured relationships for nested folders, notes, and notebooks, including foreign key constraints.

--- LunarWebApp/Data/ApplicationDbContext.cs
+++ LunarWebApp/Data/ApplicationDbContext.cs
@@ -1,13 +1,38 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using LunarWebApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LunarWebApp.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public DbSet<Notebook> Notebooks { get; set; }
+        public DbSet<Folder> Folders { get; set; }
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Notebook>()
+                .HasMany(n => n.Folders)
+                .WithOne(f => f.Notebook)
+                .HasForeignKey(f => f.NotebookId);
+
+            modelBuilder.Entity<Notebook>()
+                .HasMany(n => n.Notes)
+                .WithOne(note => note.Notebook)
+                .HasForeignKey(note => note.NotebookId);
+
+            modelBuilder.Entity<Folder>()
+                .HasMany(f => f.Folders)
+                .WithOne(pf => pf.ParentFolder)
+                .HasForeignKey(pf => pf.ParentFolderId);
+
+            modelBuilder.Entity<Folder>()
+                .HasMany(f => f.Notes)
+                .WithOne(n => n.Folder)
+                .HasForeignKey(n => n.FolderId);
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
